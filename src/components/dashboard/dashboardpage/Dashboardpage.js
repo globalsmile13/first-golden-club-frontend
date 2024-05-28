63ff2d3d8fb8dec576320b5ef7ae74d9b96bb3ff
Amaechi Chisom <amaechilegend@gmail.com>
2024-05-28 21:57:26 +0100

    deleted_at fix

--- src/components/dashboard/dashboardpage/Dashboardpage.js
+++ src/components/dashboard/dashboardpage/Dashboardpage.js
@@ -174,7 +174,7 @@ const Dashboardpage = () => {
 
         <div className='dashboard-rightbar'>
           <h2>Overview</h2>
-          {myProfile?.deleted_at || myProfile?.level_id?.level_number  == undefined ?
+          {myProfile?.profile?.deleted_at || myProfile?.level_id?.level_number  === undefined ?
           <div style={{color:'white', 
           background:'#FA0000', 
           borderRadius:'10px', 
